e8feab9422e245682ee6461616063f55754586fa
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-02-12 23:49:01 +0200

    fix: customer name

--- src/lib/saythanks.ts
+++ src/lib/saythanks.ts
@@ -162,7 +162,7 @@ export async function handleFulfillment({
       },
       body: JSON.stringify({
         customer_msisdn: metadata.phoneNumber,
-        customer_name: "-",
+        customer_name: metadata.phoneNumber,
         product_variant_id: variantId,
         send: true,
         value,
